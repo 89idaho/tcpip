c6aa260534a00e20782811499fff2aa34bae68cb
student <student@M110314>
2018-01-30 13:38:08 +0900

    Modify....

--- src/tcpip1/Client.java
+++ src/tcpip1/Client.java
@@ -1,7 +1,53 @@
 package tcpip1;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.io.OutputStreamWriter;
+import java.net.Socket;
+import java.net.UnknownHostException;
+
+//IoT에서 쓰레드를 이용함.
 public class Client {
-  public static void main(String args[]) {
-  
-  }
+	public static void main(String args[]) {	
+		String ip = "70.12.111.144";
+		int port = 7777;	
+		Socket socket= null;		
+		OutputStream outs = null;
+		OutputStreamWriter outw = null;	
+		
+		try {
+			
+			socket = new Socket(ip, port);				
+			System.out.println("Start Client ...");			
+			System.out.println("Connected OK ...");		
+			// Send Data ...
+			outs = socket.getOutputStream();
+			outw = new OutputStreamWriter(outs);
+			outw.write("모찌모찌");
+			System.out.println("Send Completed");			
+			
+			
+			
+		} catch (UnknownHostException e) {
+			System.out.println("UnknownHostException...");
+			e.printStackTrace();
+		} catch (IOException e) {
+			System.out.println("IOException...");
+			e.printStackTrace();
+		} finally {
+			try {				
+				outw.close();
+				outs.close();			
+				socket.close();
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+			
+		}
+
+	}
 }
