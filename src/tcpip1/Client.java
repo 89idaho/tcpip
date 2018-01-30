90712a3eeb3bc16237c4cb89f6cb6d7fa378c5c3
student <student@M110314>
2018-01-30 14:43:53 +0900

    14:43

--- src/tcpip1/Client.java
+++ src/tcpip1/Client.java
@@ -1,9 +1,6 @@
 package tcpip1;
 
-import java.io.BufferedReader;
 import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
 import java.io.OutputStream;
 import java.io.OutputStreamWriter;
 import java.net.Socket;
